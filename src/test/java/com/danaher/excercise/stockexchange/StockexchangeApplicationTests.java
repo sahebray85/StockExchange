9e6e4a453479613a1df7bea48399c123a5421d4e
SANKHA RAY <saheb_ray85@yahoo.co.in>
2024-04-12 21:18:35 +0200

    Initial Commit

---
+++ src/test/java/com/danaher/excercise/stockexchange/StockexchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danaher.excercise.stockexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockexchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
